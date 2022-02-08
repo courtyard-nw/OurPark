06496c74e318d8cce33bb63b35b42c1cfcd1e32f
Jihunz <thespringsong@gmail.com>
2022-02-08 22:11:55 +0900

    feat: 리뷰 등록 기능 ajax으로 교체

--- src/main/webapp/resources/js/addInfo.js
+++ src/main/webapp/resources/js/addInfo.js
@@ -1,5 +1,23 @@
 $(function() {
-
 	$(".cancel").click(() => history.back());
 
+	$("form button").click(() => add());
 })
+
+function add() {
+	const formData = new FormData($("form")[0])
+	const placeName = $("#placeName").val(); 
+
+    $.ajax("../rest/", {
+        method: "POST",
+		data: formData,
+		contentType: false,
+		processData: false,
+        success: result => {
+			location.href = "http://localhost:9090/ourpark/map/map?keyword=" + placeName;
+			return;
+        },
+        error: xhr => { alert(`오류 발생: ${xhr.statusText}`) }
+    });
+
+}
