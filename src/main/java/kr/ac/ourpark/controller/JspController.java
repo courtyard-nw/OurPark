40cd7f184744eed4ed1794a74a06cc26d5ce32ee
Jihunz <thespringsong@gmail.com>
2022-02-09 21:55:50 +0900

    feat: jsp 호출용 컨트롤러 통합 후 생성(작업중)

---
+++ src/main/java/kr/ac/ourpark/controller/JspController.java
@@ -0,0 +1,60 @@
+package kr.ac.ourpark.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.ac.ourpark.model.Place;
+import kr.ac.ourpark.model.Review;
+
+@RequestMapping("/jsp")
+@Controller
+public class JspController {
+	final String review = "review/";
+	final String map = "map/";
+	
+	@RequestMapping("/update")
+	public String update() {
+		return review + "list";
+	}
+	
+	@PostMapping("/addInfo")
+	public String addInfo(Review item, HttpSession session) {
+
+		session.setAttribute("placeAddr", item);
+
+		return review + "addInfo";
+	}
+
+	@RequestMapping("/addAddr")
+	public String addAddr() {
+		return review + "addAddr";
+	}
+	
+	@RequestMapping("/review")
+	public String review() {
+		return review + "review";
+	}
+	
+	@RequestMapping("/map")
+	public String map(String keyword, HttpSession session) {
+
+		session.setAttribute("keyword", keyword);
+
+		return map + "map";
+	}
+	
+	@ResponseBody
+	@PostMapping("/sendPlace")
+	public String review(@RequestBody Place item, HttpSession session) {
+
+		session.setAttribute("place", item);
+
+		return "sendPlace";
+	}
+
+}
