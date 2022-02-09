1ee8d74f7be1823ddb7488b3f5695a5b48769b70
Jihunz <thespringsong@gmail.com>
2022-02-09 22:54:09 +0900

    fix: regDate 표기 방식을 타임스탬프에서 일반적 표기법으로 변경

--- src/main/webapp/resources/js/list.js
+++ src/main/webapp/resources/js/list.js
@@ -1,44 +1,46 @@
-$(function() {
+$(function () {
 	$(".cancel").click(() => history.back());
 
 	list();
 })
 
 function list() {
-    $.ajax("../rest/list", {
-        method: "GET",
+	$.ajax("../rest/list", {
+		method: "GET",
 		dataType: "JSON",
-        success: result => {
+		success: result => {
 			let list = result;
-			
-			const tbody = $("tbody");
 
-			if(list.length < 1) {
+			const tbody = $("tbody");
+			let timestamp = new Date(list[0].regDate);
+			let regDate = `${timestamp.getFullYear()}-0${(timestamp.getMonth() + 1)}-0${timestamp.getDate()} ${timestamp.getHours()}:${timestamp.getMinutes()}:${timestamp.getSeconds()}`;
+
+			if (list.length < 1) {
 				const msg = $("<td>").attr("colspan", "4").addClass("text-center").text("작성한 리뷰가 없습니다");
 				tr.append(msg);
 				tbody.append(tr);
 
 				return;
 			} else {
-				for(let i=0; i<=list.length-1; i++) {
+				for (let i = 0; i <= list.length - 1; i++) {
 					let tr = $("<tr>");
 
 					let code = $("<td>").text(list[i].code);
 					let name = $("<td>").append($("<a>").attr("href", "#").text(list[i].placeName));
-					let date = $("<td>").text(list[i].regDate);
+					let date = $("<td>").text(regDate);
 
 					let btn = $("<td>");
 					let upd = $("<a>").attr("href", `update/${list[i].code}`).addClass("btn btn-secondary btn-sm").text("수정");
 					let del = $("<a>").attr("href", `delete/${list[i].code}`).addClass("btn btn-secondary btn-sm").text("삭제");
 					btn.append(upd).append(del);
 					tr.append(code).append(name).append(date).append(btn);
-					tbody.append(tr);					
-				}					
+					tbody.append(tr);
+				}
 
 			}
 
-        },
-        error: xhr => { alert(`오류 발생: ${xhr.statusText}`) }
-    });
+		},
+		error: xhr => { alert(`오류 발생: ${xhr.statusText}`) }
+	});
 
 }
