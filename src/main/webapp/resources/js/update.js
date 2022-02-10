195bd85c8a90785d0c5e39fe4d353f0ebf4426ca
Jihunz <thespringsong@gmail.com>
2022-02-10 17:28:00 +0900

    feat: 장소 코드를 가져오는 함수 생성

--- src/main/webapp/resources/js/update.js
+++ src/main/webapp/resources/js/update.js
@@ -6,6 +6,15 @@ $(function() {
 	
 })
 
+function getCode() {
+	const url = location.href;
+	const code = url.split("/");
+
+	return code[6];
+}
+
 function rate() {
 	$(`#rating3-${item.rating}`).prop("checked", true);
 }
+
+//리뷰 코드 받기 -> item() -> dom 생성
