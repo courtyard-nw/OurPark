3573da388f9e8b0ffa20241fc482c6bedaa734ff
Jihunz <thespringsong@gmail.com>
2022-02-12 22:01:21 +0900

    fix: 날짜 10이하일 때 왼쪽에 0 추가 및 날짜 데이터 탐색오류 수정

--- src/main/webapp/resources/js/list.js
+++ src/main/webapp/resources/js/list.js
@@ -12,9 +12,7 @@ function list() {
 			let list = result;
 
 			const tbody = $("tbody");
-			let timestamp = new Date(list[0].regDate);
-			let regDate = `${timestamp.getFullYear()}-0${(timestamp.getMonth() + 1)}-0${timestamp.getDate()} ${timestamp.getHours()}:${timestamp.getMinutes()}:${timestamp.getSeconds()}`;
-
+			
 			if (list.length < 1) {
 				const msg = $("<td>").attr("colspan", "4").addClass("text-center").text("작성한 리뷰가 없습니다");
 				tr.append(msg);
@@ -27,11 +25,20 @@ function list() {
 
 					let code = $("<td>").text(list[i].code);
 					let name = $("<td>").append($("<a>").attr("href", "#").text(list[i].placeName));
+
+					let timestamp = new Date(list[i].regDate);
+
+					let fmtDate = timestamp.getDate();
+					if(fmtDate < 10) {
+						fmtDate = `0${timestamp.getDate()}`;
+					}
+
+					let regDate = `${timestamp.getFullYear()}-0${(timestamp.getMonth() + 1)}-${fmtDate} ${timestamp.getHours()}:${timestamp.getMinutes()}:${timestamp.getSeconds()}`;
 					let date = $("<td>").text(regDate);
 
 					let btn = $("<td>");
 					let upd = $("<a>").attr("href", `update/${list[i].code}`).addClass("btn btn-secondary btn-sm").text("수정");
-					let del = $("<a>").attr("href", `delete/${list[i].code}`).addClass("btn btn-secondary btn-sm").text("삭제");
+					let del = $("<button>").attr("id", list[i].code).addClass("btn btn-secondary btn-sm delBtn").text("삭제");
 					btn.append(upd).append(del);
 					tr.append(code).append(name).append(date).append(btn);
 					tbody.append(tr);
