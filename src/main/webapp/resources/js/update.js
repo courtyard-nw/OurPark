0e9a5e610c670d7a3cf59996d9f07a366649121e
Jihunz <thespringsong@gmail.com>
2022-02-12 22:05:09 +0900

    feat: 리뷰 수정 기능 구현 중

--- src/main/webapp/resources/js/update.js
+++ src/main/webapp/resources/js/update.js
@@ -5,7 +5,30 @@ $(function() {
 	});
 	
 	item();
+
+	$("form button").click(() => update());
+
 })
+
+//서버에서는 formData의 값이 null임 -> 콘솔 쿼리문의 파라미터 참고
+function update() {
+	const formData = new FormData($("form")[0]);
+	const code = getCode();
+
+    $.ajax(`../../rest/${code}`, {
+        method: "PUT",
+		data: formData,
+		contentType: false,
+		processData: false,
+        success: result => {
+			alert("수정 완료");
+			location.href = `http://localhost:9090/ourpark/jsp/list`;
+			return;
+        },
+        error: xhr => { alert(`오류 발생: ${xhr.statusText}`) }
+    });
+
+}
 
 function item() {
 	const code = getCode();
